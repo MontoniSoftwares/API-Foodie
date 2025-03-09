3e903eecfc143c3a271b465e39a032272e622a67
Roberto <robertomontoni.dev@gmail.com>
2025-03-09 11:22:42 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 import router from "./routes.js";
 
+const express = require("express");
 const app = express();
+
+const PORT = process.env.PORT || 3000; // Porta dinâmica ou padrão 3000
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API funcionando!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
-
 app.listen(3001, () => {
-    console.log("Servidor rodando na porta: 3001");
+  console.log("Servidor rodando na porta: 3001");
 });
